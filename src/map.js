7c2b9a79c587c7242eb1f3217d6768de41dd7ce4
imghasemi <in.ghasemi@gmail.com>
2016-08-30 20:28:55 +0200

    Pushing the changes to this branch. Logo for Leaflet.js

--- src/map.js
+++ src/map.js
@@ -12,7 +12,7 @@ class Map {
     this.el = leaflet.map(container).setView(initialView, initialZoom);
 
     const accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw';
-	  
+
     leaflet.tileLayer(`https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${accessToken}`, {
       maxZoom: 18,
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
@@ -29,7 +29,19 @@ class Map {
       '#e74c3c', '#ecf0f1', '#2c3e50', '#f1c40f', '#1abc9c',
     ];
     this.usedColors = [];
-      this.processes = {};
+    this.processes = {};
+
+    //specifying new icon to present the anchors
+    var greenIcon = L.icon({
+      iconUrl: 'leaf-green.png',
+      //shadowUrl: 'leaf-shadow.png',
+
+      iconSize:     [38, 95], // size of the icon
+      shadowSize:   [50, 64], // size of the shadow
+      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      shadowAnchor: [4, 62],  // the same for the shadow
+      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
 
   }
 
@@ -58,16 +70,16 @@ class Map {
         <li>Duration: ${p.duration}</li>
       </ul>
       `;
-	  
+
     p.points.forEach((val, i) => {
 		const marker = leaflet.marker(val).addTo(this.el);
-		
+
 		marker.bindPopup(`Node ${i}`);
-		
+
       latlngs.push(marker.getLatLng());
       p.markers.push(marker);
     });
-	  
+
       p.line = leaflet.polyline(latlngs, { color:color, weight: 10, opacity: 0.7 }).addTo(this.el)
 		  .bindTooltip(p.info, {interactive: true, sticky: true});
 
@@ -79,9 +91,9 @@ class Map {
 	  // Why we pop twice?
       lines.pop();
       lines.pop();
-	  
+
 	  console.log(this.processes[uid].line.bindTooltip(label + '', {permanent: true}));
-	  
+
     this.processes[uid].info = `
       ${lines.join('\n')}
         ${label}
