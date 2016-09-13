7479803b5b84f5db9926f24d8a53976f50352867
s6mispru <s6mispru@uni-bonn.de>
2016-09-13 17:34:13 +0200

    #13:
    added two new metrics

--- src/queries.js
+++ src/queries.js
@@ -39,6 +39,23 @@ export default {
         '?p scor:hasMetricRL_34 ?value2 .',
       ],
     },
+    DELIVERY_IN_FULL: {
+      SELECT: '(AVG(xsd:decimal((xsd:decimal(?value1)+xsd:decimal(?value2))/2)) AS ?metricResult)',
+      TRIPLES: [
+        '?p scor:hasMetricRL_33 ?value1 .',
+        '?p scor:hasMetricRL_50 ?value2 .',
+      ],
+    },
+    PERFECT_CONDITION: {
+      SELECT: '(AVG(xsd:double((xsd:decimal(?value1)+xsd:decimal(?value2)+xsd:decimal(?value3)+xsd:decimal(?value4)+xsd:decimal(?value5))/5)) AS ?metricResult)',
+      TRIPLES: [
+        '?p scor:hasMetricRL_12 ?value1 .',
+        '?p scor:hasMetricRL_24 ?value2 .',
+        '?p scor:hasMetricRL_41 ?value3 .',
+        '?p scor:hasMetricRL_42 ?value4 .',
+        '?p scor:hasMetricRL_55 ?value5 .',
+      ],
+    },
   },
   PROPS: {
     PRODUCT_NAME: {
