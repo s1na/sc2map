d3f7275e43a5c10561fcf28a43d1f2c13a114516
Sina Mahmoodi <itz.s1na@gmail.com>
2016-09-22 13:40:46 +0200

    Minor fixes in data generator

--- data-generator.py
+++ data-generator.py
@@ -19,13 +19,13 @@ YEAR = 1 * 12 * 30 * 24 * 60 * 60
 
 # Global variables
 nodeTypes = ['factory', 'mine', 'powerPlant', 'farm', 'silo', 'windMill']
-processTypes = [scorvoc.DeliverStockedProduct, scorvoc.SourceMakeToOrderProduct]
+processTypes = [scorvoc['DeliverStockedProduct'], scorvoc['SourceMakeToOrderProduct']]
 metrics = [
-    ex['hasMetricCO_14'], ex['hasMetricCO_15'], ex['hasMetricCO_16'], ex['hasMetricCO_17'],
-    ex['hasMetricRL_32'], ex['hasMetricRL_33'], ex['hasMetricRL_34'], ex['hasMetricRL_50'],
-    ex['hasMetricRL_12'], ex['hasMetricRL_24'], ex['hasMetricRL_41'], ex['hasMetricRL_42'],
-    ex['hasMetricAG_1'], ex['hasMetricAG_2'], ex['hasMetricAG_3'], ex['hasMetricAG_4'],
-    ex['hasMetricRL_55'], ex['hasMetricAG_5'],
+    scorvoc['hasMetricCO_14'], scorvoc['hasMetricCO_15'], scorvoc['hasMetricCO_16'], scorvoc['hasMetricCO_17'],
+    scorvoc['hasMetricRL_32'], scorvoc['hasMetricRL_33'], scorvoc['hasMetricRL_34'], scorvoc['hasMetricRL_50'],
+    scorvoc['hasMetricRL_12'], scorvoc['hasMetricRL_24'], scorvoc['hasMetricRL_41'], scorvoc['hasMetricRL_42'],
+    scorvoc['hasMetricAG_1'], scorvoc['hasMetricAG_2'], scorvoc['hasMetricAG_3'], scorvoc['hasMetricAG_4'],
+    scorvoc['hasMetricRL_55'], scorvoc['hasMetricAG_5'],
 ]
 point_counter = 0
 
@@ -44,7 +44,7 @@ def datetime_generator(before=time.time() - YEAR):
 def generate_process(g, pid):
     path = generate_path(g, pid)
     process = ex['process%d' % pid]
-    g.add((process, RDF.type, Literal(processTypes[random.randint(0, len(processTypes) - 1)])))
+    g.add((process, RDF.type, processTypes[random.randint(0, len(processTypes) - 1)]))
     g.add((process, ex.hasSupplier, Literal(id_generator())))
     g.add((process, ex.isSubjectOf, Literal(id_generator())))
     g.add((process, ex.hasPath, path))
@@ -62,10 +62,10 @@ def generate_path(g, pid):
         point = ex['point%d' % point_counter]
         point_counter += 1
         g.add((point, RDF.type, geo.Point))
-        g.add((point, geo.lat, Literal(random.uniform(-90, 90), datatype=XSD.double)))
-        g.add((point, geo.long, Literal(random.uniform(-180, 180), datatype=XSD.double)))
-        g.add((point, ex.hasName, Literal(id_generator(), datatype=XSD.string)))
-        g.add((point, ex.hasType, Literal(nodeTypes[random.randint(0, len(nodeTypes) - 1)], datatype=XSD.string)))
+        g.add((point, geo.lat, Literal(str(random.uniform(-90, 90)), datatype=XSD.double)))
+        g.add((point, geo.long, Literal(str(random.uniform(-180, 180)), datatype=XSD.double)))
+        g.add((point, ex.hasName, Literal(id_generator())))
+        g.add((point, ex.hasType, Literal(nodeTypes[random.randint(0, len(nodeTypes) - 1)])))
         points.append(point)
 
     pathSeq = ex['p%dPathSeq' % pid]
